d8aaa24e2f47c27332d9ff15d43b0efddfa5d121
ahmed-nasir <nasirahmed595@gmail.com>
2022-05-21 00:14:05 +0600

    Load user for admin page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -14,9 +15,26 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    // console.log('verifyJWT working');
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized Access!' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
 
     try {
+        console.log("databage connected")
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('bookings');
@@ -27,7 +45,11 @@ async function run() {
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
 
+        app.get('/user',async(req,res)=>{
+            const users = await userCollection.find().toArray();
+            res.send(users)
         })
 
         app.put('/user/:email', async (req, res) => {
@@ -40,7 +62,10 @@ async function run() {
             };
 
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
+            //JWT
+            // const token=jwt.sign({Payload }, privateKey, { Options: expire hobe kokhon});
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ result, token });
         })
 
         app.get('/available', async (req, res) => {
@@ -77,27 +102,33 @@ async function run() {
              * app.delete('/booking/:id') //
             */
 
-            app.get('/booking', async (req, res) => {
-                const patient = req.query.patient;
+        })
+
+        app.get('/booking', verifyJWT, async (req, res) => {
+            const patient = req.query.patient;
+
+            const decodedEmail = req.decoded.email;
+
+            if (patient === decodedEmail) {
                 const query = { patient: patient };
                 const bookings = await bookingCollection.find(query).toArray();
-
                 res.send(bookings);
-            })
+            } else {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+        })
 
 
 
-            app.post('/booking', async (req, res) => {
-                const booking = req.body;
-                const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
-                const exists = await bookingCollection.findOne(query);
-                if (exists) {
-                    return res.send({ success: false, booking: exists })
-                }
-                const result = await bookingCollection.insertOne(booking);
-                return res.send({ success: true, result });
-            })
-
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
         })
     }
     finally {
